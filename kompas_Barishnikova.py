1d21b084d872533093aea6895b8ce80eab44c591
Екатерина <barishnikovaekaterina@gmail.com>
2022-01-20 00:34:53 +0200

    Add files via upload

---
+++ kompas_Barishnikova.py
@@ -0,0 +1,12 @@
+def direction(facing, turn):
+    facinglist = ['N','NE','E','SE','S','SW','W','NW']
+    x = int(turn/45)
+    facingindex = facinglist.index(facing)
+    y = facingindex + x
+    if turn == 0:
+        return(facing)
+    elif  -8 < y < 8:
+        return(facinglist[y])
+    else: 
+        z = y % 8
+        return(facinglist[z])
